21eaa69a03d232255af8cbae96fcdd1f14a99dce
Nawin <nawinwin1231@gmail.com>
2026-09-11 12:07:52 +0700

    update_11092026_rfid

--- api/master_rfid.js
+++ api/master_rfid.js
@@ -1,53 +1,142 @@
 const express = require("express");
 const router = express.Router();
-const Sequelize = require("sequelize");
-const moment = require("moment");
-//Create constance and link to model
-const visual_input_table = require("../model/visual_input");
 
+//Create constance and link to model
+const master_rfid_table = require("../model/master_rfid");
 const constance = require("../constance/constance");
 
-router.get("getMasterRfid", async (req, res) => {
+//select all
+router.get("/getMasterRfid", async (req, res) => {
   try {
-    let data = await visual_input_table.Sequelize.query(`
-        SELECT [emp]
-              ,[rfid]
-              ,[fname]
-              ,[lname]
-              ,[mfg_date]
-          FROM [turning_visual].[dbo].[master_rfid]
-          order by [emp]
+    let data = await master_rfid_table.sequelize.query(`
+      SELECT [emp], [rfid], [fname], [lname], [mfg_date]
+      FROM [turning_visual].[dbo].[master_rfid]
+      ORDER BY [emp]
     `);
+
     return res.json({ data: data[0], message: constance.result_ok });
   } catch (error) {
     return res.json({ data: [], message: constance.result_nok });
   }
 });
 
-router.post("inRFID", async (req, res) => {
+//check rfid (FORM VISUAL TURNING)
+router.post("/checkRfid", async (req, res) => {
   try {
-    let data = await visual_input_table.Sequelize.query(`
-        INSERT INTO [turning_visual].[dbo].[master_rfid] ([emp],[rfid],[mfg_date])
-        VALUES ('${req.body.emp}','${req.body.rfid}',GETDATE())`);
-    return res.json({
-      data: data[0],
-      message: constance.result_ok,
-    });
+    let rfid = req.body.rfid;
+
+    if (!rfid) {
+      return res.json({ data: "missing_field", message: constance.result_nok });
+    }
+
+    let check = await master_rfid_table.sequelize.query(`
+      SELECT [emp], [rfid], [fname], [lname]
+      FROM [turning_visual].[dbo].[master_rfid]
+      WHERE [rfid] = '${rfid}'
+    `);
+
+    if (check[0].length > 0) {
+      return res.json({ data: check[0][0], message: constance.result_ok });
+    }
+
+    // ไม่พบบัตรนี้ในระบบ
+    return res.json({ data: null, message: constance.result_nok });
   } catch (error) {
     return res.json({ data: error.message, message: constance.result_nok });
   }
 });
 
-router.post("updateRFID", async (req, res) => {
+//insert
+router.post("/inRFID", async (req, res) => {
   try {
-    let data = await visual_input_table.Sequelize.query(`
-        UPDATE [turning_visual].[dbo].[master_rfid]
-        SET rfid = '${req.body.rfid}'
-        where emp = '${req.body.emp}'
-        `);
-        return res.json({ data: data[0], message: constance.result_ok});
+    let emp = req.body.emp;
+    let rfid = req.body.rfid;
+    let fname = req.body.fname || "";
+    let lname = req.body.lname || "";
+
+    if (!emp || !rfid) {
+      return res.json({ data: "missing_field", message: constance.result_nok });
+    }
+
+    // check emp/rfid ซ้ำก่อน insert
+    let check = await master_rfid_table.sequelize.query(`
+      SELECT [emp], [rfid]
+      FROM [turning_visual].[dbo].[master_rfid]
+      WHERE [emp] = '${emp}' OR [rfid] = '${rfid}'
+    `);
+
+    if (check[0].length > 0) {
+      let is_dup_emp = check[0][0].emp === emp;
+      return res.json({
+        data: is_dup_emp ? "dup_emp" : "dup_rfid",
+        message: constance.result_nok,
+      });
+    }
+
+    let data = await master_rfid_table.sequelize.query(`
+      INSERT INTO [turning_visual].[dbo].[master_rfid] ([emp], [rfid], [fname], [lname], [mfg_date])
+      VALUES ('${emp}', '${rfid}', '${fname}', '${lname}', GETDATE())
+    `);
+
+    return res.json({ data: data[0], message: constance.result_ok });
   } catch (error) {
-    return res.json({ data: error.message, message: constance.result_nok});
+    return res.json({ data: error.message, message: constance.result_nok });
   }
 });
+
+//update
+router.post("/updateRFID", async (req, res) => {
+  try {
+    let emp = req.body.emp;
+    let rfid = req.body.rfid;
+    let fname = req.body.fname || "";
+    let lname = req.body.lname || "";
+
+    if (!emp || !rfid) {
+      return res.json({ data: "missing_field", message: constance.result_nok });
+    }
+
+    // เช็คว่า rfid นี้ถูกใช้โดยพนักงานคนอื่นอยู่แล้วหรือไม่
+    let check = await master_rfid_table.sequelize.query(`
+      SELECT [emp]
+      FROM [turning_visual].[dbo].[master_rfid]
+      WHERE [rfid] = '${rfid}' AND [emp] != '${emp}'
+    `);
+
+    if (check[0].length > 0) {
+      return res.json({ data: "dup_rfid", message: constance.result_nok });
+    }
+
+    let data = await master_rfid_table.sequelize.query(`
+      UPDATE [turning_visual].[dbo].[master_rfid]
+      SET [rfid] = '${rfid}', [fname] = '${fname}', [lname] = '${lname}'
+      WHERE [emp] = '${emp}'
+    `);
+
+    return res.json({ data: data[0], message: constance.result_ok });
+  } catch (error) {
+    return res.json({ data: error.message, message: constance.result_nok });
+  }
+});
+
+//delete
+router.post("/deleteRFID", async (req, res) => {
+  try {
+    let emp = req.body.emp;
+
+    if (!emp) {
+      return res.json({ data: "missing_field", message: constance.result_nok });
+    }
+
+    let data = await master_rfid_table.sequelize.query(`
+      DELETE FROM [turning_visual].[dbo].[master_rfid]
+      WHERE [emp] = '${emp}'
+    `);
+
+    return res.json({ data: data[0], message: constance.result_ok });
+  } catch (error) {
+    return res.json({ data: error.message, message: constance.result_nok });
+  }
+});
+
 module.exports = router;
